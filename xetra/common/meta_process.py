3c8e860cadf2e075c0b6e91cecf7115642533de5
zahur <zahurmeerun@hotmail.com>
2023-03-14 03:25:07 +0000

    add: test for s3 bucket connector

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,16 @@
+"""
+Class to process meta file
+"""
+
+class MetaProcess:
+    """
+    Class for working with meta file
+    """
+
+    @staticmethod
+    def update_meta_file(self):
+        pass
+
+    @staticmethod
+    def return_date_list(self):
+        pass
